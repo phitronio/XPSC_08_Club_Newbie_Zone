4bea219b730be2558c131c301a6d14af78526418
Hridoy Barua <143290428+Hrid897@users.noreply.github.com>
2026-06-28 23:28:51 +0600

    Update B.cpp

--- Dummy Solution/Week 9/B.cpp
+++ Dummy Solution/Week 9/B.cpp
@@ -1,68 +1,56 @@
-#include <iostream>
-#include <vector>
-using std::vector;
-using std::cin;
-using std::cout;
-void setbomb(int x, int y, int n, int m, vector<vector<int>> &ans)
-{
-	if (x > 0)
-	{
-		if (y > 0)
-			ans[x - 1][y - 1]++;
-		ans[x - 1][y]++;
-		if (y < m - 1)
-			ans[x - 1][y + 1]++;
-	}
-	if (x < n - 1)
-	{
-		if (y > 0)
-			ans[x + 1][y - 1]++;
-		ans[x + 1][y]++;
-		if (y < m - 1)
-			ans[x + 1][y + 1]++;
-	}
-	if (y > 0)
-		ans[x][y - 1]++;
-	if (y < m - 1)
-		ans[x][y + 1]++;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int run();
+
+int main() {
+#ifdef home
+  freopen("i", "r", stdin);
+  freopen("d", "w", stderr);
+#endif
+  cout.precision(15);
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  run();
 }
-int main(int argc, char *argv[])
-{
-	int n, m;
-	cin >> n >> m;
-	vector<vector<char>> v(n, vector<char>(m));
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> v[i][j];
-		}
-	}
-	vector<vector<int>> ans(n, vector<int>(m));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-		{
-			if (v[i][j] == '*')
-				setbomb(i, j, n, m, ans);
-		}
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-		{
-			if (v[i][j] == '.')
-			{
-				if (ans[i][j] != 0)
-				{
-					cout << "NO";
-					return 0;
-				}
-			}
-			else if (v[i][j] != '*')
-				if (ans[i][j] != v[i][j] - '0')
-				{
-					cout << "NO";
-					return 0;
-				}
-		}
-	cout << "YES";
-	return 0;
+
+const int N = 5000;
+
+int a[N];
+
+int dp[N][N];
+
+int run() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    dp[0][i] = a[i];
+  }
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j <= n - i; j++) {
+      dp[i][j] = dp[i - 1][j + 1] ^ dp[i - 1][j];
+    }
+  }
+
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      dp[i][j] = max({dp[i][j], dp[i - 1][j], dp[i - 1][j + 1]});
+    }
+  }
+
+  int q;
+  cin >> q;
+
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    cin >> l >> r;
+    --l;
+    int len = r - l - 1;
+    cout << dp[len][l] << '\n';
+  }
 }
